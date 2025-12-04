83106ebf3d306ebc43f5bd9d5e2d37fea332a831
Chelsea R <chelsearoopnarine@yahoo.com>
2025-12-04 18:32:46 -0400

    test print statements

--- App/views/index.py
+++ App/views/index.py
@@ -5,7 +5,11 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
 def index_page():
-    return render_template('index.html')
+      # This replaces the HTML template with a simple JSON response
+    return {
+        "status": "running",
+        "message": "Hello! Your Flask app is live!"
+    }
 
 @index_views.route('/init', methods=['GET'])
 def init():
